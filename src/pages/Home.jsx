4807bd4979a1154ba5e2d9410f3424391e08d3df
HimalBhandari05 <himalbhandari342@gmail.com>
2025-07-20 04:45:39 +0545

    tried the different method to fetch the data from an api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import Footer from "./components/footer"
 import Navbar from "./components/navbar"
 import axios from "axios"
 import { useBeforeUnload } from "react-router-dom"
+import CardDetail from "./components/CardDetail"
 
 
 // strict mode le garda chai duita aako ho. (error haru lai clear way ma <dekhauxa></dekhauxa>)
@@ -15,49 +16,61 @@ import { useBeforeUnload } from "react-router-dom"
 function Home(){
     const [loading , setLoading] = useState(true) // stores the loading status
     const [blogs , setBlogs] = useState([]); // state to store the fetched data 
+    const [blog , setBlog] = useState([]); // state to store the fetched data 
     const [selectedBlog , setSelectedBlog] = useState(null);
 
-    async function fetchBlogs(){
-        const response = await axios.get("https://687a74a3abb83744b7eceb2d.mockapi.io/api/v1/blogs")
-        if(response.status == 200){
-            setBlogs(response.data)
-            console.log(response.data)
-        }
-        else{
-            alert('data aayena')
-        }
-    }
+    // async function fetchBlogs(){
+    //     const response = await axios.get("https://687a74a3abb83744b7eceb2d.mockapi.io/api/v1/blogs")
+    //     if(response.status == 200){
+    //         setBlogs(response.data)
+    //         // console.log(response.data)
+    //     }
+    //     else{
+    //         alert('data aayena')
+    //     }
+    // }
 
     useEffect(()=>{
-        fetchBlogs();
+        // fetchBlogs();
+        fetchBlog();
     } , [])
 
 
-    // fetch('https://687a74a3abb83744b7eceb2d.mockapi.io/api/v1/blogs' , {
-    //     method: 'GET' ,
-    //     headers:{
-    //         'content-type':'application/json'
-    //     },
-    // }).then((response)=>{
-    //     console.log(response)
-    //     return response.json();
-    // }).then(tasks=>{
-    //     console.log(tasks)
-    // }).catch(error=>{
+    async function fetchBlog(){
+        try{
+            const response = await fetch('https://687a74a3abb83744b7eceb2d.mockapi.io/api/v1/blogs' , {
+                method: 'GET' ,
+                headers:{
+                    'content-type':'application/json',
+            },
+        });
+        
+        if (!response.ok){
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
 
-    // })
+        const data = await response.json()
+        setBlog(data)
+        }
+        catch (error) {
+            alert(`There was an error fetching blogs: ${error.message}`);
+            console.error('Fetch error:', error);
+        }
+    }
 
     return(
         <>      
             <Navbar />
             <div className="flex flex-wrap gap-10 p-5">
                 {
-                    blogs.map((blog)=>{
+                    blog.map((b)=>{
                         return(
-                            <Card key={blog.id} blog={blog} title= {blog.title} content={blog.content} createdAt= {blog.createdAt} subtitle={blog.subtitle}/>
+                            <Card key={b.id} blog={b}/>
                         )
                     })
                 }
+            <div>
+            </div>
             </div>
             <Footer />
         </>
