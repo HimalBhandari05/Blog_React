fd9a1c7ea329968aa140d0b9b9e9b17bcfb90e64
HimalBhandari05 <himalbhandari342@gmail.com>
2025-07-19 08:42:15 +0545

    feat: added the dynamic rendering of the cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,63 @@
 
+import { useState , useEffect } from "react"
 import logo from "../assets/images/logo.png"
 import Card from "./components/card"
 import Footer from "./components/footer"
 import Navbar from "./components/navbar"
+import axios from "axios"
+import { useBeforeUnload } from "react-router-dom"
+
+
+// strict mode le garda chai duita aako ho. (error haru lai clear way ma <dekhauxa></dekhauxa>)
+//  use effect hook ( koi maanxe le first time visit garyo or kei vayo vaney kunei function trigger garna lai chai yo use hunxa )
+// [] here is the dependency array, and the function is anynomous function and arrow function (callback function)
 
 function Home(){
+    const [loading , setLoading] = useState(true) // stores the loading status
+    const [blogs , setBlogs] = useState([]); // state to store the fetched data 
+    const [selectedBlog , setSelectedBlog] = useState(null);
+
+    async function fetchBlogs(){
+        const response = await axios.get("https://687a74a3abb83744b7eceb2d.mockapi.io/api/v1/blogs")
+        if(response.status == 200){
+            setBlogs(response.data)
+            console.log(response.data)
+        }
+        else{
+            alert('data aayena')
+        }
+    }
+
+    useEffect(()=>{
+        fetchBlogs();
+    } , [])
+
+
+    // fetch('https://687a74a3abb83744b7eceb2d.mockapi.io/api/v1/blogs' , {
+    //     method: 'GET' ,
+    //     headers:{
+    //         'content-type':'application/json'
+    //     },
+    // }).then((response)=>{
+    //     console.log(response)
+    //     return response.json();
+    // }).then(tasks=>{
+    //     console.log(tasks)
+    // }).catch(error=>{
+
+    // })
+
     return(
         <>      
             <Navbar />
             <div className="flex flex-wrap gap-10 p-5">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                {
+                    blogs.map((blog)=>{
+                        return(
+                            <Card key={blog.id} blog={blog} title= {blog.title} content={blog.content} createdAt= {blog.createdAt} subtitle={blog.subtitle}/>
+                        )
+                    })
+                }
             </div>
             <Footer />
         </>
