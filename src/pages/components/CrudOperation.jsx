5cdbf4a2e561289feeb0a5453d38c2f713d5110d
HimalBhandari05 <himalbhandari342@gmail.com>
2025-07-21 23:43:01 +0545

    feat: PUT request with form data

--- src/pages/components/CrudOperation.jsx
+++ src/pages/components/CrudOperation.jsx
@@ -1,17 +1,21 @@
 import axios from "axios"
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import Loading from "./Loading"
 
 
 function LearnCrud(){
     const navigate = useNavigate()
     const useApi = "https://687a74a3abb83744b7eceb2d.mockapi.io/api/v1/Crud"
     const [error , setError] = useState(null);
-    const [isLoading , setIsloading] = useState(false) // laoding add garna lai since fetch garna lai time laagxa
-    const [user , setUser] = useState({
-        name:"",
-        password:"",
-    })
+    const [isLoading , setIsloading] = useState(false)
+    
+
+    // laoding add garna lai since fetch garna lai time laagxa
+    // const [user , setUser] = useState({
+    //     name:"",
+    //     password:"",
+    // })
 
     // main parts include: handleInput , handleSubmit 
     /*
@@ -19,52 +23,115 @@ function LearnCrud(){
         handleSubmit : server ma data pathauna usually use hunxa.
     */
 
-    const handleInput = (event)=>{
-        event.preventDefault();
-        const { name , value } = event.target; // event.target le chai object return garxa form ko hai ta
-        console.log(name , value)
-        setUser({...user , [name]:value}) // yo chai ...user le existing state leuxa ra [name]: ma chai hamro data form ko jaaney vayo
-        console.log(user)
+    // const handleInput = (event)=>{
+    //     event.preventDefault();
+    //     const { name , value } = event.target; // event.target le chai object return garxa form ko hai ta
+    //     console.log(name , value)
+    //     setUser({...user , [name]:value}) // yo chai ...user le existing state leuxa ra [name]: ma chai hamro data form ko jaaney vayo
+    //     console.log(user)
+    // }
+
+    // const handleSubmit = async (event)=>{
+    //     event.preventDefault();
+    //     console.log(user)
+    //     try{
+    //         setIsloading(true)
+    //         const response = await axios.post(useApi, {
+    //             method:'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //             },
+    //             user
+    //         })
+    //         console.log(response)
+    //         if(response.status == 201){
+    //             console.log('Data post successfully')
+    //             navigate('/')
+    //             setUser({
+    //                 name:"",
+    //                 password:"",
+    //             })
+    //         }
+    //     }catch(error){
+    //         setError(error.message);
+    //     }
+    //     finally{
+    //         setIsloading(false);
+    //     }
+    // }
+
+    // next one is about editing the particular user , we have to use the user id here (to use the put)
+
+    const {id} = useParams();
+    const [putUser , setPutUser] = useState({
+        email:"",
+        password:"",
+    }) // this to accept the object
+
+    useEffect(()=>{
+        getUserData();
+    }, [])
+
+    const getUserData = async ()=>{
+        const response = await axios.get(useApi.concat('/') + id)
+        if(response.status == 200)
+        {
+            console.log(`Response hai getuser data ko `,response.data)
+            setPutUser(response.data)
+            // putUser.name = response.data.name
+            // putUser.password = response.data.password
+        }
+        else{
+            console.log("Data didn't came!");
+        }
     }
 
-    const handleSubmit = async (event)=>{
-        event.preventDefault();
-        console.log(user)
+
+    const handleChange = (e)=>{
+        const { name , value } = e.target;
+        console.log(name , value)
+        setPutUser({...putUser , [name]:value})
+    }
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        console.log('Submit working')
         try{
-            setIsloading(true)
-            const response = await axios.post(useApi, {
-                method:'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                user
-            })
-            console.log(response)
-            if(response.status == 201){
-                console.log('Data post successfully')
-                setUser({
-                    name:"",
-                    password:"",
-                })
+            setIsloading(true);
+            const response = await axios.put(
+                useApi.concat('/')+id,
+                putUser,
+                { headers: { 'Content-Type': 'application/json' } }
+            )
+            console.log('submit ko response ho hai ',response)
+            if(response.status == 200 ){
+                console.log('Set successfully! ')
+                navigate('/')
             }
         }catch(error){
             setError(error.message);
-        }
-        finally{
-            setIsloading(false);
+        }finally{
+            setIsloading(false); // first ma false xa ani tespaxi true banaako and then finally false banaako.
         }
     }
 
+
     return(
         <div className="max-w-md mx-auto mt-24">
+            {/* <div id="heading">
+                { 
+                    isLoading && <Loading />  // this will check if the loading is true if true then calls the loader component
+                }  
+                { error && <p> Error: {error}  </p>}
+            </div> */}
             <form className="p-4" onSubmit={handleSubmit}>
                 <div className="mb-4">
                     <label htmlFor="email" className="block text-gray-700 font-bold mb-2">Email</label>
-                    <input type="email" id="email" onChange={handleInput} name="email" value={user.email} className="w-full py-2 px-3 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:border-blue-500" placeholder="Enter your email address" />
+                    <input type="email" id="email" onChange={handleChange} name="email" value={putUser.email} className="w-full py-2 px-3 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:border-blue-500" placeholder="Enter your email" />
                 </div>
                 <div className="mb-4">
                     <label htmlFor="password" className="block text-gray-700 font-bold mb-2">Password</label>
-                    <input type="password" id="password" onChange={handleInput} name="password" value={user.password} className="w-full py-2 px-3 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:border-blue-500" placeholder="Enter your password" />
+                    <input type="password" id="password" onChange={handleChange} name="password" value={putUser.password}  className="w-full py-2 px-3 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:border-blue-500" placeholder="Enter the password" />
                 </div>
                 <div className="flex items-center justify-between">
                     <a href="#" className="text-gray-500 hover:text-blue-500 focus:outline-none focus:underline transition duration-150 ease-in-out">Forgot
